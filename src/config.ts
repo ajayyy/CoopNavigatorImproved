d6046318b0f80a5016b188fafa418fb706c200f4
Ajay Ramachandran <dev@ajay.app>
2020-01-17 23:42:48 -0500

    Added sponsorblock config code

---
+++ src/config.ts
@@ -0,0 +1,102 @@
+interface ConfigCallback {
+    (changes: chrome.storage.StorageChange): any
+}
+
+var configListeners: Array<ConfigCallback> = [];
+var localConfig;
+var config;
+
+/**
+ * A Map cannot be stored in the chrome storage. 
+ * This data will be decoded from the array it is stored in
+ * 
+ * @param {*} data 
+ */
+function decodeStoredItem(data) {
+    if(typeof data !== "string") return data;
+    
+	try {
+        let str = JSON.parse(data);
+        
+		if(!Array.isArray(str)) return data;
+		return new Map(str);
+    } catch(e) {
+
+        // If all else fails, return the data
+        return data;
+    }
+}
+
+function configProxy() {
+    chrome.storage.onChanged.addListener((changes: chrome.storage.StorageChange, namespace) => {
+        for (const key in changes) {
+            localConfig[key] = decodeStoredItem(changes[key].newValue);
+        }
+
+        for (const callback of configListeners) {
+            callback(changes);
+        }
+    });
+	
+    let handler: ProxyHandler<any> = {
+        set: function(obj, prop, value) {
+            localConfig[prop] = value;
+
+            chrome.storage.sync.set({
+                [prop]: value
+            });
+
+            return true;
+        },
+        
+        get: function(obj, prop) {
+			return obj[prop] || localConfig[prop];
+        }
+		
+    };
+
+    return new Proxy<any>({handler}, handler);
+}
+
+function fetchConfig() { 
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(null, function(items) {
+            localConfig = items;  // Data is ready
+            resolve();
+        });
+    });
+}
+
+async function setupConfig() {
+    await fetchConfig();
+    addDefaults();
+    
+	config = configProxy();
+}
+
+const defaults = {
+	"extensionEnabled": true
+}
+
+// Reset config
+function resetConfig(): void {
+	config = defaults;
+};
+
+// Add defaults
+function addDefaults(): void {
+    for (const key in defaults) {
+        if(!localConfig.hasOwnProperty(key)) {
+			localConfig[key] = defaults[key];
+		}
+    }
+};
+
+// Sync config
+setupConfig();
+
+export {
+    resetConfig,
+    config,
+    configListeners
+};
--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,39 @@
 import * as Utils from "./utils";
+import * as Config from "./config";
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init(): void {
+async function init(): Promise<void> {
     Utils.localizeHtmlPage();
+
+    await Utils.wait(() => Config.config !== undefined, 1000, 10);
+
+    // See if the buttons should be swapped
+    let disableExtensionButton = <HTMLDivElement> document.getElementById("disableExtension");
+    let enableExtensionButton = <HTMLDivElement> document.getElementById("enableExtension");
+    if (!Config.config.extensionEnabled) {
+        swapExtensionStatus(false);
+    }
+
+    // Add listener to extension button
+    disableExtensionButton.addEventListener("click", () => swapExtensionStatus());
+    enableExtensionButton.addEventListener("click", () => swapExtensionStatus());
+}
+
+/**
+ * Swaps if the extension is enable or not
+ */
+function swapExtensionStatus(changeConfig: boolean = true): void {
+    if (changeConfig) Config.config.extensionEnabled = !Config.config.extensionEnabled;
+
+    let disableExtensionButton = <HTMLDivElement> document.getElementById("disableExtension");
+    let enableExtensionButton = <HTMLDivElement> document.getElementById("enableExtension");
+
+    if (Config.config.extensionEnabled) {
+        enableExtensionButton.classList.add("hidden");
+        disableExtensionButton.classList.remove("hidden");
+    } else {
+        disableExtensionButton.classList.add("hidden");
+        enableExtensionButton.classList.remove("hidden");
+    }
 }
