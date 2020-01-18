9f5c7df1b00692c8780e2166fb04741621010d96
Ajay Ramachandran <dev@ajay.app>
2020-01-17 23:17:33 -0500

    Added better localization.

--- src/popup.ts
+++ src/popup.ts
@@ -3,14 +3,5 @@ import * as Utils from "./utils";
 window.addEventListener('DOMContentLoaded', init);
 
 function init(): void {
-    document.getElementById("startScrape").addEventListener("click", (): void => {
-        chrome.tabs.query({
-            active: true, 
-            currentWindow: true
-        }, function(tabs): void {
-            chrome.tabs.sendMessage(tabs[0].id, {
-                message: "startScrape"
-            });
-        });
-    });
+    Utils.localizeHtmlPage();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -30,8 +30,32 @@ async function wait(condition: WaitCondition, timeout = 120000, check = 100): Pr
     });
 }
 
+function localizeHtmlPage(): void {
+    //Localize by replacing __MSG_***__ meta tags
+    var objects = document.getElementsByClassName("coopNavigatorImprovedPageBody")[0].children;
+    for (var j = 0; j < objects.length; j++) {
+        var obj = objects[j];
+        
+        let localizedMessage = getLocalizedMessage(obj.innerHTML.toString());
+        if (localizedMessage) obj.innerHTML = <string> localizedMessage;
+    }
+}
+
+function getLocalizedMessage(text: string): string | boolean {
+    var valNewH: string = text.replace(/__MSG_(\w+)__/g, function(match, v1) {
+        return v1 ? chrome.i18n.getMessage(v1) : "";
+    });
+
+    if(valNewH != text) {
+        return valNewH;
+    } else {
+        return false;
+    }
+}
+
 export {
     ChromeMessage, 
     ChromeMessageCallback, 
-    wait
+    wait,
+    localizeHtmlPage
 };
